2dc256f9431e6ef5f8ef8578e3101941047667c4
*pknowledge* <mishrasangeeta944@gmail.com>
2024-04-18 19:05:10 +0530

    complete quote page

--- fronted/src/pages/Signup.tsx
+++ fronted/src/pages/Signup.tsx
@@ -1,12 +1,15 @@
- import Quote from "../components/Quote"
+// import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
 
-const Signup = () => {
-  return (
-    <div>
-
-       <Quote/>
+export const Signup = () => {
+    return <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            {/* <div>
+                <Auth type="signup" />
+            </div> */}
+            <div className="hidden lg:block">
+                <Quote />
+            </div>
+        </div>
     </div>
-  )
 }
-
-export default Signup
